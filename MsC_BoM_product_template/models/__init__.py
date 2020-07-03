82c965c146bd329df5f201e645f6848dd291c865
Pierrick Fichefet <pfi@idealisconsulting.com>
2020-07-03 15:51:20 +0200

    [REWORK] MsC 13 18 17: rename module

--- MsC_BoM_product_template/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import mrp_bom_line
-from . import mrp_bom
-from . import mrp_production
-from . import stock_move
--- MsC_variant/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product_template
-from . import product_attribute
-from . import product_attribute_value
-from . import product_template_attribute_value
