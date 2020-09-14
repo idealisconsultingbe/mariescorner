df60c5e607c1b670e1f300907be1a98dfbc98846
Pierrick Fichefet <pfi@idealisconsulting.com>
2020-09-14 12:35:21 +0200

    [ADD] Update manifest comments

--- mc_sales_lot/__manifest__.py
+++ mc_sales_lot/__manifest__.py
@@ -8,7 +8,7 @@
     'summary': 'Allow to use sales lot.',
     'description': """
     This module add the sale lots logic.
-    SO line will ask for a sale lots if their product has a tracking enable.
+    SO line will ask for a sale lots if their product has a tracking enable and if sales lot tracking is enable.
     Sale lot are created on the SO and are propagated in a UP BOTTOM way.
     It allow the user to easily see which transfer is related to which so line.
     In the case of an mto flow sale lots will impose a nomenclature for standard LOT / SN.
