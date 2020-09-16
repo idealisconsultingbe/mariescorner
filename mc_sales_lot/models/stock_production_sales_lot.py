cb80da0f0726a94b1c5bf648e73f77ba36526ae2
Pierrick Fichefet <pfi@idealisconsulting.com>
2020-09-16 14:33:51 +0200

    [IMP] add picking_ids on form view of sales lot

--- mc_sales_lot/models/stock_production_sales_lot.py
+++ mc_sales_lot/models/stock_production_sales_lot.py
@@ -18,10 +18,12 @@ class ProductionSalesLot(models.Model):
         'product.product', string='Product',
         domain="[('type', 'in', ['product', 'consu']), ('sale_ok', '=', True), '|', ('company_id', '=', False), ('company_id', '=', company_id)]", required=True, ondelete='restrict', check_company=True)
     production_ids = fields.One2many('mrp.production', 'sales_lot_id', string='Manufacturing Orders')
+    stock_move_ids = fields.One2many('stock.move', 'sales_lot_id', string='Stock Move Lines')
     stock_move_line_ids = fields.One2many('stock.move.line', 'sales_lot_id', string='Stock Move Lines')
     sale_order_line_ids = fields.One2many('sale.order.line', 'sales_lot_id', string='Sale Order Lines')
     sale_order_ids = fields.Many2many('sale.order', stirng='Sale Orders', compute='_get_sale_orders')
     lot_ids = fields.Many2many('stock.production.lot', stirng='Lot/Serial', compute='_get_lots')
+    picking_ids = fields.Many2many('stock.picking', stirng='Transfers', compute='_get_pickings')
     log_sales_lot_status_ids = fields.One2many('log.sales.lot.status', 'sales_lot_id', string='Status')
 
     def _get_sale_orders(self):
@@ -33,7 +35,14 @@ class ProductionSalesLot(models.Model):
 
     def _get_lots(self):
         """
-        :return serial at which the sale lot is linked.
+        :return serials at which the sale lot is linked.
         """
         for sale_lot in self:
             sale_lot.lot_ids = sale_lot.stock_move_line_ids.mapped('lot_id')
+
+    def _get_pickings(self):
+        """
+        :return transfers at which the sale lot is linked.
+        """
+        for sale_lot in self:
+            sale_lot.picking_ids = sale_lot.stock_move_ids.mapped('picking_id')
