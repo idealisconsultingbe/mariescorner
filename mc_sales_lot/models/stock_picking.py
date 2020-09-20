01095ba8a9aacba2acebb4f01de2b7ef1a5549f6
DWA <dwa@idealisconsulting.com>
2020-09-20 13:13:16 +0200

    Revert "[IMP] MsC 38,41 -> remove payment terms on invoice address, improve intercompany log picking"
    This reverts commit b6093e853d2dfb5db2db1a7c932222e1986764c1.

--- mc_sales_lot/models/stock_picking.py
+++ mc_sales_lot/models/stock_picking.py
@@ -12,32 +12,25 @@ class Picking(models.Model):
         Overridden method
 
         Create an entry log if
-            - there is at least one move line bound to a sales lot
+            - there is at least one move line bound to a manufacturing number
             - AND it is an intercompany transfer OR a customer delivery
         """
-
-        def is_intercompany(partner, companies):
-            """ inner function that defines if partner is part of a company """
-            if partner.id in [company['partner_id'][0] for company in companies]:
-                return True
-            elif partner.parent_id:
-                return is_intercompany(partner.parent_id, companies)
-            else:
-                return False
-
         res = super(Picking, self).action_done()
         companies = self.env['res.company'].search([]).read(['partner_id'])
         user = self.env.user
         model = self.env['ir.model'].search([('model', '=', self.env.context.get('active_model'))])
         record = self.env.context.get('active_id')
         for line in self.move_line_ids.filtered(lambda l: l.sales_lot_id):
-            if self.picking_type_code == 'incoming' and is_intercompany(self.partner_id, companies):
-                msg = _('The lot {} has been received by {}').format(line.lot_id.name, self.company_id.name)
-                line.sales_lot_id.create_log(msg, user=user, model=model, record=record)
-            elif self.picking_type_code == 'outgoing' and is_intercompany(self.partner_id, companies):
-                msg = _('The lot {} is on its way towards {} warehouse').format(line.lot_id.name, self.partner_id.name)
-                line.sales_lot_id.create_log(msg, user=user, model=model, record=record)
+            if self.picking_type_code == 'incoming' and self.partner_id.id in [company['partner_id'][0] for company in companies]:
+                name = _('Receipt OK')
+                msg = _('The lot {} have been received by {}').format(line.lot_id.name, self.company_id.partner_id.name_get()[0][1])
+                line.sales_lot_id.create_log(name, msg, user=user, model=model, record=record)
+            elif self.picking_type_code == 'outgoing' and self.partner_id.id in [company['partner_id'][0] for company in companies]:
+                name = _('Internal Delivery OK')
+                msg = _('The lot {} is on its way towards {} warehouse').format(line.lot_id.name, self.partner_id.name_get()[0][1])
+                line.sales_lot_id.create_log(name, msg, user=user, model=model, record=record)
             elif self.picking_type_code == 'outgoing':
-                msg = _('The lot {} has been delivered to customer {}').format(line.lot_id.name, self.partner_id.name)
-                line.sales_lot_id.create_log(msg, user=user, model=model, record=record)
+                name = _('Delivery OK')
+                msg = _('The lot {} have been delivered to customer {}').format(line.lot_id.name, self.partner_id.name_get()[0][1])
+                line.sales_lot_id.create_log(name, msg, user=user, model=model, record=record)
         return res
