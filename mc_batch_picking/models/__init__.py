8d9db445b8f89a1cdeabbb3de3df0cdb93f083bd
Pierrick Fichefet <pfi@idealisconsulting.com>
2021-02-04 20:57:58 +0100

    [IMP] MC Planned Batch Picking List

--- mc_batch_picking/models/__init__.py
+++ mc_batch_picking/models/__init__.py
@@ -7,3 +7,4 @@ from . import stock_location
 from . import purchase_order
 from . import stock_move_line
 from . import stock_production_lot
+from . import stock_picking
--- mc_batch_picking/report/mc_batch_picking_custom_report.py
+++ mc_batch_picking/report/mc_batch_picking_custom_report.py
@@ -4,24 +4,15 @@
 from odoo import api, models, _
 
 
-class BatchPickingReport(models.Model):
-    _name = 'report.mc_batch_picking.batch_packing_list_report_template'
+class PlannedBatchPickingReport(models.Model):
+    _name = 'report.mc_batch_picking.planned_batch_packing_lst_report'
 
     def _get_report_values(self, docids, data=None):
-        report = self.env['ir.actions.report']._get_report_from_name('mc_batch_picking.batch_packing_list_report_template')
+        report = self.env['ir.actions.report']._get_report_from_name('mc_batch_picking.planned_batch_packing_lst_report')
         batch_pickings = self.env[report.model].browse(docids)
         batch_pickings_by_carrier = {}
         for batch in batch_pickings:
-            pickings_by_carrier = {}
-            for picking in batch.picking_ids:
-                if picking.carrier_id and picking.carrier_id.name in pickings_by_carrier:
-                    pickings_by_carrier[picking.carrier_id.name] |= picking
-                elif picking.carrier_id:
-                    pickings_by_carrier[picking.carrier_id.name] = picking
-                else:
-                    if 'Undefined' not in pickings_by_carrier:
-                        pickings_by_carrier['Undefined'] = self.env['stock.picking']
-                    pickings_by_carrier['Undefined'] |= picking
+            pickings_by_carrier = batch.picking_ids.get_picking_by_carrier()
             batch_pickings_by_carrier[batch.id] = pickings_by_carrier
         return {
             'doc_ids': self._ids,
@@ -31,22 +22,49 @@ class BatchPickingReport(models.Model):
             'batch_pickings_by_carrier': batch_pickings_by_carrier,
         }
 
+
+class PlannedPickingShipmentReport(models.Model):
+    _name = 'report.mc_batch_picking.planned_batch_packing_lst_document'
+
+    def _get_report_values(self, docids, data=None):
+        report = self.env['ir.actions.report']._get_report_from_name('mc_batch_picking.planned_batch_packing_lst_document')
+        pickings = self.env[report.model].browse(docids)
+        pickings_by_carrier = pickings.get_picking_by_carrier()
+        return {
+            'doc_ids': self._ids,
+            'doc_model': report.model,
+            'docs': pickings,
+            'data': data,
+            'pickings_by_carrier': pickings_by_carrier,
+        }
+
+
+class BatchPickingReport(models.Model):
+    _name = 'report.mc_batch_picking.batch_packing_list_report_template'
+
+    def _get_report_values(self, docids, data=None):
+        report = self.env['ir.actions.report']._get_report_from_name('mc_batch_picking.batch_packing_list_report_template')
+        batch_pickings = self.env[report.model].browse(docids)
+        batch_pickings_by_carrier = {}
+        for batch in batch_pickings:
+            pickings_by_carrier = batch.picking_ids.get_picking_by_carrier()
+            batch_pickings_by_carrier[batch.id] = pickings_by_carrier
+        return {
+            'doc_ids': self._ids,
+            'doc_model': report.model,
+            'docs': batch_pickings,
+            'data': data,
+            'batch_pickings_by_carrier': batch_pickings_by_carrier,
+        }
+
+
 class PickingShipmentReport(models.Model):
     _name = 'report.mc_batch_picking.batch_packing_list_document'
 
     def _get_report_values(self, docids, data=None):
         report = self.env['ir.actions.report']._get_report_from_name('mc_batch_picking.batch_packing_list_document')
         pickings = self.env[report.model].browse(docids)
-        pickings_by_carrier = {}
-        for picking in pickings:
-            if picking.carrier_id and picking.carrier_id.name in pickings_by_carrier:
-                pickings_by_carrier[picking.carrier_id.name] |= picking
-            elif picking.carrier_id:
-                pickings_by_carrier[picking.carrier_id.name] = picking
-            else:
-                if 'Undefined' not in pickings_by_carrier:
-                    pickings_by_carrier['Undefined'] = self.env['stock.picking']
-                pickings_by_carrier['Undefined'] |= picking
+        pickings_by_carrier = pickings.get_picking_by_carrier()
         return {
             'doc_ids': self._ids,
             'doc_model': report.model,
