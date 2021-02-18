6eee89a7e42e589dd0683e01d0524bcf623d8aff
Pierrick Fichefet <pfi@idealisconsulting.com>
2021-02-18 21:17:37 +0100

    [IMP] Update scipts

--- scripts/set_product_short_description_lines.py
+++ scripts/set_product_short_description_lines.py
@@ -6,42 +6,110 @@ Configure the short description for every product belonging to a specific catego
 import argparse
 import logging
 
-primary_fabric = {
-    'en': 'Primary fabric:',
-    'fr_BE': 'Tissu primaire:',
+labels = {
+    1: {
+        'en': 'Primary fabric:',
+        'fr_BE': 'Tissu primaire:',
+    },
+    2: {
+        'en': 'Secondary fabric:',
+        'fr_BE': 'Tissu secondaire:',
+    },
+    'color': {
+        'en': '/ Color:',
+        'fr_BE': '/ Couleur:',
+    },
+    3: {
+        'en': '',
+        'fr_BE': '',
+    },
+    4: {
+        'en': 'Mc Care:',
+        'fr_BE': 'Mc Care:',
+    },
+    5: {
+        'en': ' Leather covering:',
+        'fr_BE': ' Leather covering:',
+    },
+    6: {
+        'en': 'Fireproof treatment:',
+        'fr_BE': 'Fireproof treatment:',
+    },
+    7: {
+        'en': 'Contrast Piping:',
+        'fr_BE': 'Contrast Piping:',
+    },
+    8: {
+        'en': 'Contrast fabrics:',
+        'fr_BE': 'Contrast fabrics:',
+    },
+    9: {
+        'en': 'Contrast pillow:',
+        'fr_BE': 'Contrast pillow:',
+    },
+    10: {
+        'en': 'Cushion configuration:',
+        'fr_BE': 'Cushion configuration:',
+    },
+    11: {
+        'en': 'Color of the nails:',
+        'fr_BE': 'Color of the nails:',
+    },
+    12: {
+        'en': 'Base in RAL Finish:',
+        'fr_BE': 'Base in RAL Finish:',
+    },
+    13: {
+        'en': 'Lacquered finish base:',
+        'fr_BE': 'Lacquered finish base:',
+    },
+    14: {
+        'en': 'Customer wood color',
+        'fr_BE': 'Customer wood color:',
+    },
+    15: {
+        'en': 'Feet upholstered:',
+        'fr_BE': 'Feet upholstered:',
+    },
+    16: {
+        'en': ' Feet and basement upholstered:',
+        'fr_BE': 'Feet and basement upholstered:',
+    },
+    17: {
+        'en': 'One size modification:',
+        'fr_BE': 'One size modification:',
+    },
+    18: {
+        'en': 'Fabrics with a repeat pattern:',
+        'fr_BE': 'Fabrics with a repeat pattern:',
+    },
+    19: {
+        'en': 'Fabrics width < 140 cm:',
+        'fr_BE': 'Fabrics width < 140 cm:',
+    },
+    20: {
+        'en': 'COM fabrics with a repeat pattern:',
+        'fr_BE': 'COM fabrics with a repeat pattern:',
+    },
+    21: {
+        'en': 'Artificial & Real leathers:',
+        'fr_BE': 'Artificial & Real leathers:',
+    },
+    22: {
+        'en': 'Stain Treatment:',
+        'fr_BE': 'Stain Treatment:',
+    },
 }
 
-secondary_fabric = {
-    'en': 'Secondary fabric:',
-    'fr_BE': 'Tissu secondaire:',
-}
-
-color = {
-    'en': '/ Color:',
-    'fr_BE': '/ Couleur:',
-}
-
-foot = {
-    'en': '',
-    'fr_BE': '',
-}
-
-mc_care = {
-    'en': 'Mc Care:',
-    'fr_BE': 'Mc Care:',
-}
+attribute_ids = {4: 71, 5: 72, 6: 75, 7: 76, 8: 77, 9: 78, 10: 79, 11: 80,
+                 12: 81, 13: 82, 14: 83, 15: 84, 16: 85, 17: 86, 18: 87, 19: 88, 20: 89, 21: 90, 22: 91,}
 
 def prepare_description_values(description_line, fabric_sequence, attribute_id=False, attribute_color_id=False):
     need_color = True
-    if fabric_sequence == 1:
-        label = primary_fabric
-    elif fabric_sequence == 2:
-        label = secondary_fabric
-    elif fabric_sequence == 3:
-        label = foot
-    else:
-        label = mc_care
+    if fabric_sequence not in [1, 2, 3]:
         need_color = False
+    color = labels['color']
+    label = labels[fabric_sequence]
     values = [
         {
             'type': 'attribute' if attribute_id else 'text',
@@ -61,7 +129,7 @@ def prepare_description_values(description_line, fabric_sequence, attribute_id=F
 
 def translate_values(values, lang):
     for value in values:
-        for label in [primary_fabric, secondary_fabric, color, foot]:
+        for label in labels:
             if value.text == label['en']:
                 value.with_context(lang=lang).write({'text': label[lang]})
 
@@ -74,7 +142,6 @@ parser.add_argument('secondary_fabric_id')
 parser.add_argument('secondary_fabric_color_id')
 parser.add_argument('foot_id')
 parser.add_argument('foot_color_id')
-parser.add_argument('mc_care_id')
 args = parser.parse_args()
 
 session.open(db=args.database)
@@ -95,18 +162,22 @@ logging.info('Start Creating Short Description Lines.')
 product_not_updated_description_ids = []
 for product in products:
     if not product.description_line_ids:
-        first_line = session.env['product.configurator.description.line'].create({'product_tmpl_id': product.id, 'sequence': 1})
-        second_line = session.env['product.configurator.description.line'].create({'product_tmpl_id': product.id, 'sequence': 2})
-        third_line = session.env['product.configurator.description.line'].create({'product_tmpl_id': product.id, 'sequence': 3})
-        fourth_line = session.env['product.configurator.description.line'].create({'product_tmpl_id': product.id, 'sequence': 4})
-        values = session.env['product.configurator.description.line.value'].create(prepare_description_values(first_line, 1, int(args.primary_fabric_id), int(args.primary_fabric_color_id)))
-        translate_values(values, 'fr_BE')
-        values = session.env['product.configurator.description.line.value'].create(prepare_description_values(second_line, 2, int(args.secondary_fabric_id), int(args.secondary_fabric_color_id)))
-        translate_values(values, 'fr_BE')
-        values = session.env['product.configurator.description.line.value'].create(prepare_description_values(third_line, 3, int(args.foot_id), int(args.foot_color_id)))
-        translate_values(values, 'fr_BE')
-        values = session.env['product.configurator.description.line.value'].create(prepare_description_values(fourth_line, 4, int(args.mc_care_id)))
-        translate_values(values, 'fr_BE')
+        lines = session.env['product.configurator.description.line']
+        for i in range(1, 23):
+            lines |= session.env['product.configurator.description.line'].create({'product_tmpl_id': product.id, 'sequence': i})
+        for line in lines:
+            if line.sequence == 1:
+                values = session.env['product.configurator.description.line.value'].create(prepare_description_values(line, line.sequence, int(args.primary_fabric_id), int(args.primary_fabric_color_id)))
+                translate_values(values, 'fr_BE')
+            elif line.sequence == 2:
+                values = session.env['product.configurator.description.line.value'].create(prepare_description_values(line, line.sequence, int(args.secondary_fabric_id), int(args.secondary_fabric_color_id)))
+                translate_values(values, 'fr_BE')
+            elif line.sequence == 3:
+                values = session.env['product.configurator.description.line.value'].create(prepare_description_values(line, line.sequence, int(args.foot_id), int(args.foot_color_id)))
+                translate_values(values, 'fr_BE')
+            else:
+                values = session.env['product.configurator.description.line.value'].create(prepare_description_values(line, line.sequence, attribute_ids[line.sequence]))
+                translate_values(values, 'fr_BE')
     else:
         product_not_updated_description_ids.append(product.id)
 
