410370c0c14f6c2b68e8045d8c368c332f29e4e1
Pierrick Fichefet <pfi@idealisconsulting.com>
2021-03-03 15:47:38 +0100

    [IMP] Allow to modify receipt date on PO even after validation

--- mc_sale/models/purchase_order.py
+++ mc_sale/models/purchase_order.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Idealis Consulting. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import api, models, fields
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    date_planned = fields.Datetime(tracking=True)
 
     @api.model
     def _prepare_sale_order_line_data(self, line, company, sale_id):
