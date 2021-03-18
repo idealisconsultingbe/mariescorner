52deee6db4062edd59832ef8047038daa6e5dcd1
Pierrick Fichefet <pfi@idealisconsulting.com>
2021-03-18 21:07:12 +0100

    [IMP] Allow revision on SO with validated move

--- sale_revision_history/models/sale_order.py
+++ sale_revision_history/models/sale_order.py
@@ -10,14 +10,29 @@ class SaleOrder(models.Model):
     old_revision_ids = fields.One2many('sale.order', 'current_revision_id', 'Old revisions', readonly=True, context={'active_test': False})
     revision_number = fields.Integer('Revision', copy=False)
     active = fields.Boolean('Active', default=True, copy=True)
-    
+
+    def force_action_cancel(self):
+        """
+        Allow to cancel an SO even if some move have been validated.
+        """
+        pickings = self.mapped('picking_ids')
+        picking_map_so = {}
+        for picking in pickings:
+            picking_map_so[picking.id] = picking.sale_id.id
+        pickings.write({'sale_id': False})
+        self.action_cancel()
+        picking_not_done = pickings.filtered(lambda p: p.state != 'done')
+        picking_not_done.action_cancel()
+        for picking in pickings:
+            picking.write({'sale_id': picking_map_so[picking.id]})
+
     def action_revision(self):
         self.ensure_one()
         view_ref = self.env['ir.model.data'].get_object_reference('sale', 'view_order_form')
         view_id = view_ref and view_ref[1] or False,
         rev = self.with_context(sale_revision_history=True).copy()
         (self | self.old_revision_ids).write({'current_revision_id': rev.id})
-        self.action_cancel()
+        self.force_action_cancel()
         return {
             'type': 'ir.actions.act_window',
             'name': _('Sales Order'),
