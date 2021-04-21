ca527f32f9e8caf116b0a0f8343ec4113fba3fea
DWA <dwa@idealisconsulting.com>
2021-04-21 07:12:49 +0200

    [IMP] MsC-167 -> direct download stock report

--- mc_stock_layout/wizard/print_stock_report.py
+++ mc_stock_layout/wizard/print_stock_report.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Idealis Consulting. See LICENSE file for full copyright and licensing details.
+import base64
 from odoo import fields, models, _
 
 
@@ -16,6 +17,17 @@ class PrintStockReport(models.TransientModel):
         filename = '[{}]{} - {}.pdf'.format(self.company_id.name, self.picking_id.name, self.picking_id.partner_id.name)
         data = dict(company=self.company_id)
         pdf, ext = self.action_report_id.render_qweb_pdf(self.picking_id.id, data)
-        self.picking_id.message_post(body=(_('{} has been generated with {} header/footer.')).format(self.action_report_id.name, self.company_id.name), attachments=[(filename, pdf)])
 
-
+        attachment = self.env['ir.attachment'].create({
+                            'name': filename,
+                            'type': 'binary',
+                            'datas': base64.encodestring(pdf),
+                            'res_model': self.picking_id._name,
+                            'res_id': self.picking_id.id
+                        })
+        self.picking_id.message_post(body=(_('{} has been generated with {} header/footer.')).format(self.action_report_id.name, self.company_id.name), attachment_ids=[attachment.id])
+        return {
+                    'type': 'ir.actions.act_url',
+                    'url': '/web/content/{}?download=true'.format(attachment.id),
+                    'target': 'self',
+                }
